3d903b56b29c9d5b3d8f84c5ab468a42a986a2a8
Nikita Ramzin <rivorn73@gmail.com>
2023-10-19 21:56:15 +0300

    config update

--- Prototype/server/config.py
+++ Prototype/server/config.py
@@ -1,19 +1,27 @@
 import os
 import sys
-
 from configparser import ConfigParser
 from csv_generator import generate_csv
 
-config = "config.ini"
+config_file = "config.ini"
 
-if (len(sys.argv) > 1):
-    config = sys.argv[1]
+if len(sys.argv) > 1:
+    config_file = sys.argv[1]
 
-filepath = os.path.join(os.getcwd(), config)
+filepath = os.path.join(os.getcwd(), "config.ini")
 conf = ConfigParser()
-conf.read(filepath, encoding='utf-8')
 
-directories = dict(conf.items('directories'))
+# Read the configuration file
+if os.path.exists(filepath):
+    conf.read(filepath, encoding='utf-8')
+else:
+    raise FileNotFoundError(f"Configuration file not found: {filepath}")
+
+# Check if 'directories' section is present
+if conf.has_section('directories'):
+    directories = dict(conf.items('directories'))
+else:
+    raise ValueError("Configuration file does not have the 'directories' section.")
 
 csv_file_path = os.path.join(directories['database'], 'data.csv')
 input_folder = directories['raw_images']
